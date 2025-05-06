6c8c31f5efc34f67714e4d60bfa0976148d73de4
hanapiko <apikojuma94@gmail.com>
2025-05-06 23:24:13 +0300

    modify handler to return json response

--- handlers/comment.go
+++ handlers/comment.go
@@ -171,7 +171,6 @@ func GetCommentsHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-// [Keep all your existing GetCommentsForPost, GetCommentReplies, GetUserIdFromSession, and GetPostByID functions exactly as they were]
 func jsonError(w http.ResponseWriter, message string, statusCode int) {
     w.WriteHeader(statusCode)
     json.NewEncoder(w).Encode(map[string]string{
